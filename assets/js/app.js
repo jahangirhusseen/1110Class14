a307550d8536046afbeafa24afb2990993b54c2c
Jahangir Hussen <45209967+jahangirhusseen@users.noreply.github.com>
2022-01-21 21:02:17 +0600

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,52 +1,54 @@
-const clock = document.querySelector('.clock');
+ const fiverr_area = document.querySelector('.fiverr_area');
+ const fiverr_counter = document.querySelector('.fiverr_counter');
 
-setInterval(function(){
 
-    let date = new Date();
-    let h = date.getHours();
-    let m = date.getMinutes();
-    let s = date.getSeconds();
-    clock.innerHTML = `${ h > 12 ? (h-12) == 0 ? 12 : (h - 12): h} :  ${ zeroTomi(m)} : ${zeroTomi(s)} ${ h >= 12 ? 'PM' : 'AM'}`;
+
+
+
+
+
+ fiverr_area.addEventListener('submit', function(e){
+    e.preventDefault(); 
+    let date = this.querySelector('input[type="date"]').value; 
+    let time = this.querySelector('input[type="time"]').value; 
+    
+
+
+    
+setInterval(() => { 
+    let start_time = new Date();
+    let end_time = new Date(date + ' ' + time); 
+
+    let time_diff = Math.floor(Math.abs(end_time.getTime() -  start_time.getTime()));
+    
+    let total_sec = Math.floor(time_diff / 1000);
+    let total_min = Math.floor(total_sec / 60);
+    let total_hours = Math.floor(total_min / 60);
+    let total_day = Math.floor(total_hours / 24);  
+
+    
+    let hours = total_hours - (total_day * 24);
+    let min = total_min - (total_day * 24 * 60) - (hours * 60);
+    let sec = total_sec - (total_day * 24 * 60 * 60) - (hours * 60 *60) - (min * 60);
+
+    fiverr_counter.innerHTML = ` ${ total_day > 9 ? total_day : '0'+total_day } : ${ hours > 9 ? hours : '0'+hours} : ${ min > 9 ? min : '0'+min} : ${ sec > 9 ? sec : '0'+sec} `;
+    
+
+}, 1000);
 
 });
-
 
 
 const todo = document.querySelector('#do');
 const add = document.querySelector('#add');
 const list = document.querySelector('#list');
 
-add.addEventListener('click',function(){
+add.addEventListener('click',function(e){
+e.preventDefault()
+
     let li = document.createElement('li');
     li.className = 'list-group-item';
     li.innerHTML = todo.value;
     list.appendChild(li);
     todo.value = '';
-});
-
-
-
-const start = document.querySelector('#go');
-const output = document.querySelector('#output');
-const stop = document.querySelector('#stop');
-const count = document.querySelector('#count');
-const loader = document.querySelector('.loader');
-
-
-let counter_value;
-let counter;
-
-start.addEventListener('click', () => {
-    counter_value = count.value;
-    
-
-    counter = setInterval(() => {
-        output.innerHTML = counter_value;
-        
-        if(counter_value == 0){
-            clearInterval(counter)
-        }
-        
-        counter_value--;
-    },1000);
 });
